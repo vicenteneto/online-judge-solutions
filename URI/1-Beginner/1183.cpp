d0aad6b4907e1b84eb5b8128afbcb2b5729d6687
Vicente Neto <vicente.silva@embedded.ufcg.edu.br>
2016-02-20 21:35:38 -0300

    Added 1183, 1184, 1185, 1186 and 1187 solutions

---
+++ URI/1-Beginner/1183.cpp
@@ -0,0 +1,25 @@
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	double matriz[12][12], soma = 0, resultado = 0;
+	char o;
+	cin >> o;
+
+	for (int i = 0; i < 12; i++)
+		for (int j = 0; j < 12; j++)
+			cin >> matriz[i][j];
+
+	for (int i = 0; i < 12; i++)
+		for (int j = 0; j < 12; j++)
+			soma += (j > i ? matriz[i][j] : 0);
+
+	resultado = (o == 'S' ? soma : soma / 66.0);
+
+	cout << setprecision(1) << fixed << resultado << endl;
+
+	return 0;
+}
