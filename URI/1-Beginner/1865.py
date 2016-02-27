8babc18ed7c1ef4ede0591dbf96245f9f60de32c
Vicente Neto <vicente.silva@embedded.ufcg.edu.br>
2016-02-27 17:11:21 -0300

    Added 1865 and 1866 solutions

---
+++ URI/1-Beginner/1865.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+for i in range(int(raw_input())):
+    print 'Y' if raw_input()[:4] == 'Thor' else 'N'
---
+++ URI/1-Beginner/1866.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+for i in range(int(raw_input())):
+    print 0 if int(raw_input()) % 2 == 0 else 1
